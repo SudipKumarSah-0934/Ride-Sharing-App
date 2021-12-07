de891b2770993cc88c11fe847fc5af9ad4d947c5
SudipKumarSah-0934 <bsse0934@iit.du.ac.bd>
2021-12-07 14:50:05 +0600

    third

---
+++ Rating_service/Server3.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const http=require('http');
+
+const app=express();
+const port=8004;
+const server=http.createServer(app);
+
+var MongoClient = require('mongodb').MongoClient;
+
+app.use(
+    express.urlencoded({
+      extended: true
+    })
+  );
+
+app.use(express.json());
+
+app.post('/rating',(req,res)=>
+{
+
+  console.log(req.body.DriverName +" "+req.body.Rating);
+  MongoClient.connect('mongodb://mongodb:27017/distributed-systems-project',{useUnifiedTopology: true}, function (err, client) {
+  if (err) throw err
+
+  var db = client.db('distributed-systems-project');
+  var myobj =
+   { DriverName: req.body.DriverName ,
+     Rating: req.body.Rating
+   };
+
+  db.collection("Rating").insertOne(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 data inserted");
+    
+  });
+
+});
+
+});
+
+server.listen(port,()=>
+{
+    console.log("Server is listening on localhost:"+port);
+});                            
+ 
